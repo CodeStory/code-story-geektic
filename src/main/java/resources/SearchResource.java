6b920ddc4e0569e1e9a4497c953fea0df20eade8
David Gageot <david@gageot.net>
2013-03-22 18:24:39 +0100

    Memoize

--- src/main/java/resources/SearchResource.java
+++ src/main/java/resources/SearchResource.java
@@ -1,5 +1,7 @@
 package resources;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import geeks.Geek;
@@ -14,21 +16,28 @@ import java.util.List;
 
 @Path("/search")
 public class SearchResource extends AbstractResource {
+  private Supplier<Geek[]> geeksSupplier;
+
+  public SearchResource() {
+    geeksSupplier = Suppliers.memoize(new Supplier<Geek[]>() {
+      @Override
+      public Geek[] get() {
+        return new Gson().<Geek[]>fromJson(read(file("geeks.json")), Geek[].class);
+      }
+    });
+  }
+
   @GET
   @Produces("application/json;charset=UTF-8")
   public List<Result> json(@QueryParam("q") String term) {
     List<Result> results = Lists.newArrayList();
 
-    for (Geek geek : readGeeks()) {
+    for (Geek geek : geeksSupplier.get()) {
       if (geek.matches(term)) {
         results.add(geek.toResult());
       }
     }
 
     return results;
-  }
-
-  private Geek[] readGeeks() {
-    return new Gson().<Geek[]>fromJson(read(file("geeks.json")), Geek[].class);
   }
 }
