07bedcac4af6962de07ccb5c8883f42bd7b3f06d
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2013-03-28 18:26:20 +0100

    Simplify using Twitter stream adapter

--- src/main/java/twitter/GeektickHashTagListener.java
+++ src/main/java/twitter/GeektickHashTagListener.java
@@ -4,13 +4,14 @@ import com.google.inject.Inject;
 import twitter4j.FilterQuery;
 import twitter4j.StallWarning;
 import twitter4j.Status;
+import twitter4j.StatusAdapter;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-public class GeektickHashTagListener implements StatusListener {
+public class GeektickHashTagListener extends StatusAdapter {
 
   @Inject
   private TwitterCommands twitterCommands;
@@ -20,10 +21,6 @@ public class GeektickHashTagListener implements StatusListener {
 		String accesstokensecret = System.getenv("ACCESSTOKENSECRET");
 		String consumerkey = System.getenv("CONSUMERKEY");
 		String consumersecret = System.getenv("CONSUMERSECRET");
-		System.out.println(accesstoken);
-		System.out.println(accesstokensecret);
-		System.out.println(consumerkey);
-		System.out.println(consumersecret);
 
 		TwitterStream twitterStream = new TwitterStreamFactory(
       new ConfigurationBuilder()
@@ -41,25 +38,6 @@ public class GeektickHashTagListener implements StatusListener {
   @Override
 	public void onStatus(Status status) {
     twitterCommands.onTweet(status);
-	}
-
-	@Override
-	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-		System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-	}
-
-	@Override
-	public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-		System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-	}
-
-	@Override
-	public void onScrubGeo(long userId, long upToStatusId) {
-		System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-	}
-
-	@Override
-	public void onStallWarning(StallWarning stallWarning) {
 	}
 
 	@Override
