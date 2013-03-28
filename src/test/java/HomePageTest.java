7edae4b69dde62206951c1a79716c0e77252aa7c
David Gageot <david@gageot.net>
2013-03-28 13:19:14 +0100

    Extract common code

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,26 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 
 public class HomePageTest extends AbstractPageTest {
+  @Before
+  public void go_to_homepage() {
+    goTo("/");
+  }
+
   @Test
   public void should_display_homepage() {
-    goTo("/");
-
     assertThat(title()).isEqualTo("CodeStory - Geektic");
   }
 
   @Test
   public void should_display_no_geeks() {
-    goTo("/");
-
     assertThat(find("#geeks").getText()).isEmpty();
   }
 
   @Test
   public void should_search_java_geeks() {
-    goTo("/");
-
     fill("#searchInput").with("java");
 
     assertThat(find("#geeks").getText()).contains("David");
@@ -28,8 +28,6 @@ public class HomePageTest extends AbstractPageTest {
 
   @Test
   public void should_search_scala_geeks() {
-    goTo("/");
-
     fill("#searchInput").with("scala");
 
     assertThat(find("#geeks").getText()).contains("Martin");
@@ -37,14 +35,10 @@ public class HomePageTest extends AbstractPageTest {
 
   @Test
   public void should_clear_search_field() {
-    goTo("/");
-
     fill("#searchInput").with("java");
-
     await().until("#geeks").withText().contains("David");
 
     clear("#searchInput");
-
     await().until("#geeks").withText().equalTo("");
   }
 }
